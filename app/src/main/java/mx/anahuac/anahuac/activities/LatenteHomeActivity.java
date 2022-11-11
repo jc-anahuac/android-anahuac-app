d3250d66ea665495d30796c2ac58cf4c69324f4e
nikkiihenrii <henri.niks7@gmail.com>
2022-11-10 19:45:47 -0500

    Update Latente

--- app/src/main/java/mx/anahuac/anahuac/activities/LatenteHomeActivity.java
+++ app/src/main/java/mx/anahuac/anahuac/activities/LatenteHomeActivity.java
@@ -3,14 +3,36 @@ package mx.anahuac.anahuac.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import mx.anahuac.anahuac.R;
 
 public class LatenteHomeActivity extends AppCompatActivity {
 
+    private Button btnConocenos;
+    private Button btnClick;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_latente_home);
+
+        btnConocenos = findViewById(R.id.btn_conocenos);
+        btnClick = findViewById(R.id.btn_click);
+
+        btnConocenos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
