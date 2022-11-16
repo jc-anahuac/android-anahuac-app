ea3ee5131b61ddb81e72e205d2466abb0d40325e
Diego A. Medina <79292091+archivogogo@users.noreply.github.com>
2022-11-15 19:32:15 -0500

    Adapter & Activities
    Adapter de detalle de licenciaturas y actividades de cada fragmento.

--- app/src/main/java/mx/anahuac/anahuac/activities/BachelorDetailActivity.java
+++ app/src/main/java/mx/anahuac/anahuac/activities/BachelorDetailActivity.java
@@ -1,16 +1,27 @@
 package mx.anahuac.anahuac.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import mx.anahuac.anahuac.R;
+import mx.anahuac.anahuac.adapters.BachelorDetailAdapter;
 
 public class BachelorDetailActivity extends AppCompatActivity {
+
+    ViewPager viewPagerBachelorDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_bachelor_profile);
+        setContentView(R.layout.activity_bachelor_detail);
+
+        viewPagerBachelorDetail = findViewById(R.id.vp_details);
+
+        viewPagerBachelorDetail.setAdapter(new BachelorDetailAdapter(getSupportFragmentManager()));
+
     }
+
+
 }
---
+++ app/src/main/java/mx/anahuac/anahuac/fragments/BachelorStudyFragment.java
@@ -0,0 +1,14 @@
+package mx.anahuac.anahuac.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import mx.anahuac.anahuac.R;
+
+public class BachelorStudyFragment extends Fragment {
+
+    public BachelorStudyFragment(){
+        super (R.layout.fragment_bachelor_studyplan);
+    }
+
+
+}
