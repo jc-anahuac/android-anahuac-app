1429b0ba9bce88af2596c7ce4e787be6dd509d28
Jose Cardenas <jose_cardenas@anahuac.mx>
2022-11-23 16:15:37 -0500

    fix button contact and scroll in bachelor detail

--- app/src/main/java/mx/anahuac/anahuac/activities/BachelorDetailActivity.java
+++ app/src/main/java/mx/anahuac/anahuac/activities/BachelorDetailActivity.java
@@ -2,21 +2,12 @@ package mx.anahuac.anahuac.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
-
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import com.google.android.material.tabs.TabLayout;
-
 import mx.anahuac.anahuac.R;
 import mx.anahuac.anahuac.adapters.BachelorDetailAdapter;
 
 public class BachelorDetailActivity extends AppCompatActivity {
-
-    Button contact;
 
     ViewPager viewPagerBachelorDetail;
     TabLayout tabLayout;
@@ -25,19 +16,11 @@ public class BachelorDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bachelor_detail);
-        contact = findViewById(R.id.btn_contact_activities);
 
 
         viewPagerBachelorDetail = findViewById(R.id.vp_details);
         tabLayout = findViewById(R.id.tl_TableDetail);
         viewPagerBachelorDetail.setAdapter(new BachelorDetailAdapter(getSupportFragmentManager()));
-
-        contact.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goLink("https://www.anahuac.mx/cancun/contacto");
-            }
-        });
 
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
@@ -47,10 +30,12 @@ public class BachelorDetailActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) {}
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
 
             @Override
-            public void onTabReselected(TabLayout.Tab tab) {}
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
         });
 
         viewPagerBachelorDetail.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
@@ -61,16 +46,13 @@ public class BachelorDetailActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
 
             @Override
-            public void onPageScrollStateChanged(int state) {}
+            public void onPageScrollStateChanged(int state) {
+            }
         });
-    }
-
-    private void goLink(String s) {
-        Uri uri = Uri.parse(s);
-        startActivity(new Intent(Intent.ACTION_VIEW,uri));
     }
 }
 
--- app/src/main/java/mx/anahuac/anahuac/fragments/BachelorProfileFragment.java
+++ app/src/main/java/mx/anahuac/anahuac/fragments/BachelorProfileFragment.java
@@ -1,13 +1,40 @@
 package mx.anahuac.anahuac.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import mx.anahuac.anahuac.R;
 
 public class BachelorProfileFragment extends Fragment {
 
+    Button contact;
+
     public BachelorProfileFragment(){
         super(R.layout.fragment_bachelor_profile);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        contact = view.findViewById(R.id.btn_contact_profile);
+
+        contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goLink("https://www.anahuac.mx/cancun/contacto");
+            }
+        });
+    }
+
+    private void goLink(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
+    }
 }
