f97ea16cc5db142c854d0cfdcc5bba7b31910106
Diego A. Medina <79292091+archivogogo@users.noreply.github.com>
2022-11-23 20:40:16 -0500

    Cambios Diseño y Boton de Back
    Cambios en Diseño de Fragmentos y Botones de Back en cada uno de ellos, + boton funcional en contacto.

--- app/src/main/java/mx/anahuac/anahuac/activities/BachelorDetailActivity.java
+++ app/src/main/java/mx/anahuac/anahuac/activities/BachelorDetailActivity.java
@@ -1,8 +1,14 @@
 package mx.anahuac.anahuac.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
 import com.google.android.material.tabs.TabLayout;
 import mx.anahuac.anahuac.R;
 import mx.anahuac.anahuac.adapters.BachelorDetailAdapter;
@@ -11,6 +17,8 @@ public class BachelorDetailActivity extends AppCompatActivity {
 
     ViewPager viewPagerBachelorDetail;
     TabLayout tabLayout;
+    ActionBar actionBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +30,12 @@ public class BachelorDetailActivity extends AppCompatActivity {
         tabLayout = findViewById(R.id.tl_TableDetail);
         viewPagerBachelorDetail.setAdapter(new BachelorDetailAdapter(getSupportFragmentManager()));
 
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
+
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPagerBachelorDetail.setCurrentItem(tabLayout.getSelectedTabPosition());
             }
@@ -54,5 +65,6 @@ public class BachelorDetailActivity extends AppCompatActivity {
             }
         });
     }
+
 }
 
--- app/src/main/java/mx/anahuac/anahuac/fragments/BachelorStudyFragment.java
+++ app/src/main/java/mx/anahuac/anahuac/fragments/BachelorStudyFragment.java
@@ -1,14 +1,40 @@
 package mx.anahuac.anahuac.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import mx.anahuac.anahuac.R;
 
 public class BachelorStudyFragment extends Fragment {
 
+    Button contact;
+
     public BachelorStudyFragment(){
-        super (R.layout.fragment_bachelor_studyplan);
+        super(R.layout.fragment_bachelor_studyplan);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        contact = view.findViewById(R.id.btn_contact_study);
 
+        contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goLink("https://www.anahuac.mx/cancun/contacto");
+            }
+        });
+    }
+
+    private void goLink(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
+    }
 }
